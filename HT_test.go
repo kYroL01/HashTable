480e33f0897098cac86b9cd9209785f96fff97ff
kYroL01 <michelecampus5@gmail.com>
2021-11-07 19:39:02 +0100

    ADD: test for Remove function HT

--- HT_test.go
+++ HT_test.go
@@ -21,7 +21,7 @@ func TestInsert(t *testing.T) {
 	}{
 		{"input sequence #1", [2]int{1, 10}, [2]int{1, 10}},
 		{"input sequence #2", [2]int{100, 33}, [2]int{100, 33}},
-		{"input sequence #3", [2]int{5, 999}, [2]int{5, 999}},
+		{"input sequence #3", [2]int{5, 9}, [2]int{5, 9}},
 	}
 
 	HT := NewHashMap()
@@ -31,20 +31,39 @@ func TestInsert(t *testing.T) {
 			HT.Insert(testC.input[0], testC.input[1])
 			val, b := HT.Get(testC.expected[0])
 			if b == false {
-				t.Errorf("Err 1 - Wrong value inserted compared to the input!")
+				t.Errorf("Err 1 Insert - Key not found!")
 			} else if val != testC.expected[1] {
-				t.Errorf("Err 2 - Wrong value inserted compared to the input!")
-			} else {
-				HT.PrintHM()
+				t.Errorf("Err 2 insert - <key-value> added but value is not the one expected!")
 			}
 		})
 	}
-}
-
-func TestGet(t *testing.T) {
-	//TODO
+	HT.PrintHM()
 }
 
 func TestRemove(t *testing.T) {
-	//TODO
+	// Testing Remove function
+	testCases := []struct {
+		description string
+		input       [2]int
+		expected    [2]int
+	}{
+		{"input sequence #1", [2]int{1, 10}, [2]int{1, 10}},
+		{"input sequence #2", [2]int{100, 33}, [2]int{100, 33}},
+		{"input sequence #3", [2]int{5, 999}, [2]int{5, 999}},
+	}
+
+	HT := NewHashMap()
+
+	for _, testC := range testCases {
+		t.Run(testC.description, func(t *testing.T) {
+			HT.Insert(testC.input[0], testC.input[1])
+			val, b := HT.Remove(testC.expected[0])
+			if b == false {
+				t.Errorf("Err 1 Remove - Key not found!")
+			} else if val != testC.expected[1] {
+				t.Errorf("Err 2 Remove - <key-value> removed but value is not the one expected!!")
+			}
+		})
+	}
+	HT.PrintHM()
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/kYroL01/HashTable
 
-go 1.13
+go 1.17
